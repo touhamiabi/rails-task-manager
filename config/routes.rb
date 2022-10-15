dbe99fd9d46beeaaac66c8e1584982809b52043d
touhami abi <abitouhami26@gmail.com>
2022-10-15 16:28:57 -0400

    Rails Day Two Challenge Two completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'tasks/list'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/task", to: "tasks#list"
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "/tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
